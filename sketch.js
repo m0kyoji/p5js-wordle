bded724b08a12bf6fe985afa93bf277473fd5636
kyoji <moriguchi@fundely.co.jp>
2024-01-26 20:13:55 +0900

    答えの追加

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
 WORD_LENGTH = 5;
+MAX_CHALLENGE = 10;
 
 slotList = [ "", "", "", "", "" ]
 
-collectList = [
-    "a", "l", "l", "o", "w"
-]
+collect = "Ahead,Apple,Alive,Angel,Album,Adapt,Amber,Angry,Alarm,Apart,Award,Asset,Ample,Agree,Alive,Aisle,Adult,Abide,Adept,Abode,Amigo,Amaze,Arena,Abate,Adapt,Ankle,Abide,Annoy,Aloof,Adore,Alive,Adopt,Addle,Actor,Amass,Avoid,Abide,Avert,Await,Adapt,Anode,Abash,Awake,Agile,Abyss,Amigo,Amuse,Aside,Anion,Allay,Adapt,Adorn,Admit,Amend,Abide,Ample,Amigo,Agile,Awake,Adore,Aside,Amass,Adopt,Addle,Adapt,Agony,Alloy,Acute,Alias,Ankle,Abide,Adieu,Admit,Amigo,Agree,Adore,Ample,Apart,Alive,Adorn,Ankle,Apply,Agile,Aware,Amaze,Avert,Abide,Adapt,Ample,Awake,Actor,Allow,Ankle,Amigo,Asset,Addle,Adore,Alive,Agree,Album,Brave,Bloom,Bliss,Beach,Blend,Block,Bread,Break,Basic,Brass,Brush,Brief,Brave,Blame,Brick,Bride,Brown,Burst,Build,Blend,Blush,Booth,Blaze,Bliss,Blend,Brisk,Bread,Bound,Brine,Bunch,Blame,Brute,Birth,Brisk,Bluff,Break,Brain,Broad,Blaze,Badge,Braid,Broth,Blare,Blink,Bliss,Brine,Brave,Broom,Brisk,Brief,Bread,Black,Blink,Blurt,Bride,Brush,Broad,Blaze,Block,Barge,Brisk,Blend,Brief,Brown,Blush,Brisk,Blaze,Brush,Birth,Blend,Brine,Blaze,Booth,Break,Blend,Brisk,Blame,Brown,Bread,Basic,Blare,Bride,Brisk,Bloom,Break,Brush,Blame,Birth,Brisk,Broth,Blaze,Bread,Blush,Brine,Blink,Broad,Blaze,Block,Brisk,Bloom,Catch,Cheer,Clear,Close,Crash,Craft,Chain,Chose,Chill,Cider,Climb,Crisp,Crown,Coast,Clamp,Cloud,Click,Coral,Curve,Civic,Clasp,Chalk,Creak,Comet,Crash,Crush,Child,Crisp,Chief,Craft,Count,Charm,Chant,Cough,Crave,Carry,Crude,Creek,Class,Crumb,Clean,Clock,Crime,Cheer,Churn,Camel,Clout,Curry,Court,Choir,Crate,Color,Chime,Clamp,Craft,Cigar,Cling,Cower,Crust,Click,Crisp,Cross,Charm,Chord,Cloud,Crest,Curve,Chain,Coach,Crane,Civil,Civic,Crash,Chose,Cloud,Crisp,Chord,Cider,Creak,Click,Clamp,Chess,Cower,Cheer,Clear,Crack,Cleft,Crate,Crisp,Cloud,Crash,Clamp,Churn,Creak,Crime,Count,Child,Climb,Craft,Crown,Dance,Dream,Drink,Drive,Diary,Diver,Dodge,Delta,Drill,Demon,Debut,Diary,Dwelt,Dirty,Drown,Diver,Draft,Drama,Drift,Drier,Drink,Dryer,Drawn,Denim,Defer,Douse,Droll,Dwelt,Dense,Dodge,Drown,Dairy,Drape,Dozen,Diver,Dealt,Doing,Draft,Douse,Drift,Diver,Drill,Debit,Drift,Dairy,Daily,Dicey,Deity,Digit,Drown,Disco,Drift,Dream,Dodge,Douse,Drift,Drawl,Dairy,Dealt,Douse,Draft,Drone,Diary,Drift,Drill,Dodge,Dicey,Dairy,Defer,Dream,Douse,Drift,Dodge,Diver,Diary,Drawl,Drink,Drain,Daily,Drift,Dodge,Drill,Diver,Draft,Dairy,Douse,Deity,Dicey,Drone,Drift,Diary,Dwelt,Drink,Dodge,Douse,Daily,Drain,Dairy,Drift,Dream,Early,Earth,Eagle,Email,Every,Empty,Event,Edgey,Eight,Entry,Elite,Error,Elect,Evoke,Exact,Exile,Excel,Erase,Equal,Ember,Enact,Erupt,Equip,Endow,Emote,Eager,Evoke,Elude,Enemy,Erase,Emery,Edged,Emery,Effet,Endow,Eject,Emote,Eerie,Elide,Emery,Echoe,Emote,Evoke,Elide,Edify,Evert,Elbow,Enzym,Elide,Excel,Edify,Every,Emery,Elite,Enzym,Erase,Elide,Emote,Entry,Excel,Elect,Edged,Emery,Evert,Elbow,Enzym,Evoke,Enter,Elide,Emery,Event,Edgey,Excel,Enzym,Erase,Every,Elude,Ember,Evoke,Eagle,Emery,Elect,Elide,Erase,Evert,Elite,Event,Early,Edgey,Every,Excel,Emote,Enzym,Equal,Eagle,Empty,Erase,Evert,Event,Excel,Funny,Fresh,Flame,Field,Force,Flood,Flour,Frown,Flock,Faith,Forge,Fleet,Fable,False,Flare,Funky,Frost,Flute,Fungi,Frown,Flaky,Flair,Flute,Fluff,Folly,Funny,Final,Found,Front,Flyer,Funky,Frown,Flare,Fungi,Flute,Funny,Forge,Frown,Fable,Fungi,Flash,Flare,Funny,Frost,Fleet,Flaky,Frown,Flute,Found,Fluff,Force,Frown,Flaky,Flair,Fleet,Funny,Fungi,Fable,Frown,Front,Flute,Found,Folly,Final,Flare,Funny,Forge,Fluff,Fleet,Frown,Frost,Funny,Front,Flaky,Flock,Flood,False,Funny,Flair,Frown,Flute,Fungi,Funny,Frown,Fluff,Front,Flaky,Flute,Funny,Forge,Folly,Final,Flair,Fable,Fleet,Funny,Front,Forge,Funny,Frown,Grape,Grown,Glide,Ghost,Grasp,Green,Guest,Great,Group,Grind,Greet,Genre,Glaze,Gears,Giant,Grime,Greet,Giddy,Gravy,Gloom,Gears,Grown,Grind,Ghost,Globe,Glide,Grace,Grass,Gaffe,Groan,Gator,Greed,Grime,Glint,Girth,Grasp,Greet,Green,Gully,Grind,Gloom,Glide,Ghost,Gears,Grime,Greet,Grasp,Glide,Grown,Group,Grape,Great,Gears,Ghost,Glide,Gravy,Grime,Greet,Grind,Green,Grown,Glide,Ghost,Group,Greet,Grind,Gator,Grace,Grief,Groan,Grown,Grasp,Green,Greet,Glide,Ghost,Grind,Giant,Glare,Gully,Grime,Greet,Grown,Green,Gravy,Glide,Ghost,Group,Grind,Greet,Giant,Grown,Glide,Green,Grind,Grasp,Greet,Glide,Ghost,Grape"
+collectList = collect.split(',')
 
 resultList = []
 
 let t = 0;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
@@ -19,7 +19,7 @@ function draw() {
   t ++;
   
   for (let i = 0; i < WORD_LENGTH; i++) {
-    let x = 160 + i * 60;
+    let x = 160 + i * 90;
     let y = 100;
     
     if ( t < 10) {
@@ -29,12 +29,12 @@ function draw() {
     noFill()
     stroke(100);
     strokeWeight(2);
-    rect(x, y, 50, 50);
+    rect(x, y, 80, 80);
     
     textAlign(CENTER, CENTER);
-    textSize(32);
+    textSize(40);
     fill(200);
-    text(slotList[i].toUpperCase(), x + 25, y + 25);
+    text(slotList[i].toUpperCase(), x + 40, y + 40);
   }
   
   if (resultList.length != 0) {
@@ -43,9 +43,9 @@ function draw() {
         let x = 160 + k * 60;
         let y = 160 + j * 60;
         
-        if (slotList[j][k] == collectList[k]) {
+        if (resultList[j][k] == collectList[k]) {
           fill(97, 163, 186);
-        } else if (isInclude(slotList[j][k], collectList)) {
+        } else if (isInclude(resultList[j][k], collectList)) {
           fill(210, 222, 50)
         } else {
           noFill();
@@ -68,6 +68,11 @@ function draw() {
     textSize(32);
     fill(255);
     text("CLEAR", 300, 350);
+  } else if (isOver()) {
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    fill(255);
+    text("GAME OVER", 300, 350);
   }
 }
 
@@ -90,7 +95,8 @@ function keyPressed() {
     }
   }
   if (key == 'Enter') {
-    resultList[resultList.length] = slotList
+    resultList.reverse()[resultList.length] = slotList;
+    resultList = resultList.reverse();
     slotList = [ "", "", "", "", "" ];
   }
 }
@@ -106,6 +112,11 @@ function isClear() {
       return false;
     }
   }
+  return true;
+}
+
+function isOver() {
+  if (resultList.length >= MAX_CHALLENGE)
   return true;
 }
 
